bb9f8cbc4b932b1dc64c1de98cc9c4261e100740
Trevor Page <trevor@craftycodr.com>
2019-06-23 14:07:39 -0400

    Starting to use Spring MVC

---
+++ MarsRoverApi/src/main/java/com/coderscampus/HomeController.java
@@ -0,0 +1,13 @@
+package com.coderscampus;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+  @GetMapping("/")
+  public String getHomeView () {
+    return "index";
+  }
+}
