1bc343539c001868c72baac017342f4423c6ee3a
Trevor Page <trevor@craftycodr.com>
2019-06-23 14:40:38 -0400

    Spring Model deep dive

--- MarsRoverApi/src/main/java/com/coderscampus/HomeController.java
+++ MarsRoverApi/src/main/java/com/coderscampus/HomeController.java
@@ -1,13 +1,23 @@
 package com.coderscampus;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
   @GetMapping("/")
-  public String getHomeView () {
+  public String getHomeView (ModelMap model) {
+    model.put("name", "Trevor Page");
+    model.put("address", "123 Fake St.");
+    return "index";
+  }
+  
+  @GetMapping("/testing")
+  public String getTestingView (ModelMap model) {
+    model.put("name", "John Doe");
+    model.put("address", "1234 Fake St.");
     return "index";
   }
 }
