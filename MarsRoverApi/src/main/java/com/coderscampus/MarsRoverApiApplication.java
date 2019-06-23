8abc05a1c8b90b2f117f2ce16a1b342e1d16b692
Trevor Page <trevor@craftycodr.com>
2019-06-23 14:04:25 -0400

    Initial check-in

---
+++ MarsRoverApi/src/main/java/com/coderscampus/MarsRoverApiApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoverApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRoverApiApplication.class, args);
+	}
+
+}
