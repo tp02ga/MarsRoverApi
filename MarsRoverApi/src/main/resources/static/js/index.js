5c839e6799f877158f15401d97514470aec00b76
Trevor Page <trevor@craftycodr.com>
2019-06-25 10:14:41 -0400

    Using JS to Add Events to Buttons

--- MarsRoverApi/src/main/resources/static/js/index.js
+++ MarsRoverApi/src/main/resources/static/js/index.js
@@ -1,3 +1,7 @@
-document.getElementById('curiosity').addEventListener('click', function () {
-	alert('curiosity was clicked')
-})
+let marsApiButtons = document.querySelectorAll("button[id*='marsApi']")
+
+marsApiButtons.forEach( button => button.addEventListener('click', function () {
+									const buttonId = this.id
+									const roverId = buttonId.split('marsApi')[1]
+									alert(roverId)
+								  }))
